9d486cace90a6aaed2a9eb92354177d63ea4a9d1
LUCAS DIONISIO MEDEIROS DOS SANTOS <lucas.dionisio@aluno.senac.rio>
2025-11-24 21:44:30 -0300

    aula nova novo modulo

--- virtual/dados12.py
+++ virtual/dados12.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('ClassicDisco.csv')
+'''df = pd.read_csv('ClassicDisco.csv')
 
 #Filtrar músicas lançadas depois de 1980, mostre apenas as colunas 'Year' e 'Track'
-print(df[df['Year']> 1980][['Year', 'Track']])
+print(df[df['Year']> 1980][['Year', 'Track']])'''
+
+dados = [10, 20, 30, 40]
+#Criar série
+serie = pd.Series(dados, index=['A', 'B', 'C', 'D'])
+print(serie)
